04bc3598afd4d0be33c26f6ac2cf84e2d1cc6169
Cameron Lakenen <lakenen@box.com>
2014-10-31 15:05:10 -0700

    very rough first pass to fix #14

--- hn.user.js
+++ hn.user.js
@@ -5,6 +5,87 @@
 // @match      https://news.ycombinator.com/*
 // ==/UserScript==
 
+
+var cssTemplate = '.{{id}} ~ .{{id}}, .{{id}} .comment { display: none }';
+var stylesheet = createStylesheet().sheet;
+
 [].forEach.call(document.querySelectorAll('a'), function (a) {
-  a.setAttribute('target', '_blank')
+  a.setAttribute('target', '_blank');
+});
+
+var root = { level: -1, children: [] }, prevNode = root;
+[].forEach.call(document.querySelectorAll('img[src="s.gif"]'), function (el) {
+  var row = getNearest(getNearest(el, 'table'), 'tr');
+  var span = getNearest(el, 'span');
+  if (span && span.classList.contains('pagetop')) return;
+  if (row) {
+    var node = {
+      level: el.width / 40,
+      children: [],
+      el: row
+    };
+    var parentNode = prevNode;
+    while (parentNode.level >= node.level) {
+      parentNode = parentNode.parent;
+    }
+    node.parent = parentNode;
+    parentNode.children.push(node);
+    prevNode = node;
+    var a = document.createElement('a');
+    a.addEventListener('click', collapse);
+    a.innerHTML = '[-]';
+    a.href = 'javascript:;';
+    row.querySelector('.comhead').innerHTML += ' | ';
+    row.querySelector('.comhead').appendChild(a);
+
+    var index, collapsed = false;
+    function collapse() {
+      if (collapsed) {
+        deleteCSSRule(stylesheet, index);
+      } else {
+        index = appendCSSRule(stylesheet, cssTemplate.replace(/\{\{id\}\}/g, node.id));
+      }
+      collapsed = !collapsed;
+      a.innerHTML = collapsed ? '[+]' : '[-]';
+    }
+  }
 })
+
+addCollapseClasses(root, '')
+
+function addCollapseClasses(obj, classes) {
+  obj.children.forEach(function (child, i) {
+    var id = 'comments-' + i + '-' + child.level
+    var newClasses = classes + ' ' + id
+    child.id = id
+    newClasses.trim().split(' ').forEach(function (cls) {
+      child.el.classList.add(cls)
+    })
+    addCollapseClasses(child, newClasses)
+  })
+}
+
+function getNearest(el, tag) {
+  tag = tag.toUpperCase();
+  do {
+    if (el.nodeName === tag) {
+      return el;
+    }
+  } while (el = el.parentNode);
+  return null;
+}
+
+
+function createStylesheet() {
+  var styleEl = document.createElement('style');
+  document.getElementsByTagName('head')[0].appendChild(styleEl);
+  return styleEl;
+}
+
+function appendCSSRule(sheet, rule) {
+  return sheet.insertRule(rule, sheet.cssRules.length);
+}
+
+function deleteCSSRule(sheet, index) {
+  sheet.deleteRule(index);
+}
